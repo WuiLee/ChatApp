0dbbd5e85f9eeff568506cc85f3f165e84ad484d
WuiLee <mushroom.star99@gmail.com>
2019-06-22 20:51:11 +0800

    Useful Contact Design

---
+++ app/src/main/java/com/example/chatapp/SliderAdapter.java
@@ -0,0 +1,76 @@
+package com.example.chatapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context){
+        this.context = context;
+    }
+
+    public int[] slide_images = {
+            R.drawable.doctor,
+            R.drawable.security,
+            R.drawable.nurse
+    };
+
+    public String[] slide_headings = {
+            "Mental Health Crisis Hotline",
+            "Sunway Security Emergency Hotline",
+            "Nightingale Bay / Nurse"
+    };
+
+    public String[] slide_phone_no = {
+            "Call : 018 - 389 3220",
+            "Call : 03 - 7491 8777",
+            "Call : 03 - 7491 8670"
+    };
+
+    public String[] slide_office_hours = {
+            "Office Hours : 24 hours",
+            "Office Hours : 24 hours",
+            "Office Hours : 8.30 a.m. - 5.30 p.m."
+    };
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return view == (RelativeLayout) o;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_contact_list, container, false);
+
+        ImageView slideImageView= (ImageView)view.findViewById(R.id.slide_image);
+        TextView slideHeading = (TextView) view.findViewById(R.id.slide_headings);
+        TextView slidePhoneNo = (TextView) view.findViewById(R.id.slide_phone_no);
+        TextView slideOfficeHour = (TextView) view.findViewById(R.id.slide_office_hours);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView ((RelativeLayout)object);
+    }
+}
--- app/src/main/java/com/example/chatapp/UsefulContactList.java
+++ app/src/main/java/com/example/chatapp/UsefulContactList.java
@@ -1,22 +1,77 @@
 package com.example.chatapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class UsefulContactList extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+    private TextView[] mDots;
+    private SliderAdapter sliderAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_useful_contact_list);
 
+        mSlideViewPager = (ViewPager) findViewById(R.id.view_useful_contact);
+        mDotLayout = (LinearLayout) findViewById(R.id.contact_linear_layout);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
         mToolbar = (Toolbar) findViewById(R.id.useful_contact_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Useful Contacts");
+
     }
+
+    public void addDotsIndicator(int position){
+        mDots = new TextView[3];
+        for (int i = 0; i < mDots.length; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+
+            mDotLayout.addView(mDots[i]);
+
+        }
+        if (mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorTransparentWhite));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener(){
+
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+
+            addDotsIndicator(i);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 }
