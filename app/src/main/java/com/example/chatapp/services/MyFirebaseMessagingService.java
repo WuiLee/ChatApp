3f59aed499511a4a8d38e76b7aadad77150f48b2
Hock Shem Chong <hockshem@apptivitylab.com>
2019-06-23 19:17:53 +0800

    Storing the new token to user database whenever it's refreshed, updated strings xml

--- app/src/main/java/com/example/chatapp/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/chatapp/services/MyFirebaseMessagingService.java
@@ -1,7 +1,13 @@
 package com.example.chatapp.services;
 
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.example.chatapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -33,6 +39,22 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onNewToken(String s) {
         super.onNewToken(s);
         Log.d(TAG, "Refreshed token: " + s);
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(getString(R.string.dbnode_users))
+                .child(getString(R.string.dbnode_users_uid))
+                .child(getString(R.string.dbnode_users_userToken))
+                .setValue(s)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Successfully stored new token");
+                        } else {
+                            Log.w(TAG, "Failed to store new token");
+                        }
+                    }
+                });
     }
 
     // Called when there was an error sending an upstream message.
